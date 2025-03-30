47a96bf14d78180a0960dabed98c11aac1caa25a
Eric Simpson <eric.gr.simpson@gmail.com>
2025-03-30 10:48:44 -0400

    chore: start refactoring in disparate services

---
+++ src/core/ports/base.py
@@ -0,0 +1,26 @@
+import typing_extensions as T
+
+from src.core.domain.portfolio import Portfolio
+
+
+class PortfolioRepository(T.Protocol):
+    """Abstract base class for portfolio repositories."""
+
+    def add_portfolio(self, portfolio: Portfolio) -> None:
+        """Add a portfolio to the repository.
+
+        Args:
+            portfolio (Portfolio): The portfolio to add.
+        """
+        raise NotImplementedError
+
+    def get_portfolio(self, name: str) -> Portfolio:
+        """Get a portfolio by name.
+
+        Args:
+            name (str): The name of the portfolio.
+
+        Returns:
+            Portfolio: The portfolio with the specified name.
+        """
+        raise NotImplementedError
