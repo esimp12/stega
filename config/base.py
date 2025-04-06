0ff289d62de321749b9a9b9c5a64eaf85b4851a8
Eric Simpson <eric.gr.simpson@gmail.com>
2025-04-06 10:11:40 -0400

    create service repo interaction for portfolio service

--- config/base.py
+++ config/base.py
@@ -4,20 +4,37 @@ from __future__ import annotations
 
 import os
 import typing as T
+from enum import Enum
 
 from config.frozen import FrozenConfig, FrozenConfigMeta
 
 
-def create_config(env: T.Optional[str] = None) -> BaseConfig:
+class ConfigType(Enum):
+    """Enum for config types."""
+
+    BASE = "base"
+    PORTFOLIO = "portfolio"
+
+
+def create_config(
+    env: T.Optional[str] = None,
+    config_type: ConfigType = ConfigType.BASE,
+) -> T.Union[BaseConfig, BasePortfolioConfig]:
     """Creates a new config instance.
 
     Args:
         env: A string of the environment name to load.
+        config_type: A ConfigType enum value indicating the type of config to create.
 
     Returns:
-        A BaseConfig instance.
+        A Config instance.
     """
-    return BaseConfig.create_config(env)
+    if config_type not in ConfigType:
+        raise ValueError(f"'{config_type}' is not a valid config type")
+
+    if config_type == ConfigType.BASE:
+        return BaseConfig.create_config(env)
+    return BasePortfolioConfig.create_config(env)
 
 
 def source_config_value(
@@ -98,9 +115,62 @@ class BaseConfig(FrozenConfig):
             A BaseConfig instance.
         """
         if env is None:
-            env = os.getenv("STEGA", "base")
+            env = os.getenv("STEGA_ENV", "base")
         env = env.title()
         config = cls.__CONFIGS.get(f"{env}Config", BaseConfig)
+        if config is None:
+            raise ValueError(f"{env} not a valid config")
+        return config()  # type: ignore
+
+
+class BasePortfolioConfig(FrozenConfig):
+    """See FrozenConfig.
+
+    Attributes:
+        __CONFIGS: A dict of config names to config classes.
+    """
+
+    # pylint: disable=too-few-public-methods
+
+    __CONFIGS: T.Dict[str, FrozenConfigMeta] = {}
+
+    STEGA_PORTFOLIO_ENV: str
+
+    STEGA_PORTFOLIO_REPO_TYPE: str = "postgres"
+
+    STEGA_PORTFOLIO_DB_HOST: str = "db"
+    STEGA_PORTFOLIO_DB_PORT: int = 5432
+    STEGA_PORTFOLIO_DB_USER: str
+    STEGA_PORTFOLIO_DB_PASSWORD: str
+    STEGA_PORTFOLIO_DB_NAME: str = "stega_portfolio"
+
+    def __init_subclass__(cls, *args, **kwargs):
+        """Registers subclass config.
+
+        Args:
+            *args: Args to supply to config subclass initialization.
+            **kwargs: Kwargs to supply to config subclass initialization.
+
+        Returns:
+            A BaseConfig subclass.
+        """
+        cls.__CONFIGS[cls.__name__] = cls
+        return super().__init_subclass__(*args, **kwargs)
+
+    @classmethod
+    def create_config(cls, env: T.Optional[str] = None) -> BasePortfolioConfig:
+        """Creates a config from a environment name key.
+
+        Args:
+            env: A string of the name of the config environment to load. If None, then
+                a 'base' config is created.
+
+        Returns:
+            A BaseConfig instance.
+        """
+        if env is None:
+            env = os.getenv("STEGA_PORTFOLIO_ENV", "base")
+        config = cls.__CONFIGS.get(f"{env.title()}PortfolioConfig", BasePortfolioConfig)
         if config is None:
             raise ValueError(f"{env} not a valid config")
         return config()  # type: ignore
--- src/portfolio/ports/base.py
+++ src/portfolio/ports/base.py
@@ -1,6 +1,13 @@
+from __future__ import annotations
+
 import typing_extensions as T
 
+from config.base import BasePortfolioConfig, ConfigType, create_config
 from src.portfolio.domain import Portfolio
+from src.portfolio.ports.registry import (
+    PORTFOLIO_REPO_REGISTRY,
+    PortfolioRepositoryType,
+)
 
 
 class PortfolioRepository(T.Protocol):
@@ -24,3 +31,28 @@ class PortfolioRepository(T.Protocol):
             Portfolio: The portfolio with the specified name.
         """
         raise NotImplementedError
+
+    @classmethod
+    def create_repo(
+        cls,
+        repo_type_str: str,
+        config: T.Optional[BasePortfolioConfig] = None,
+    ) -> PortfolioRepository:
+        """Create a portfolio repository of the specified type.
+
+        Args:
+            repo_type_str (str): The type of portfolio repository to create.
+
+        Returns:
+            PortfolioRepository: An instance of the specified portfolio repository.
+        """
+        if config is None:
+            config = T.cast(
+                BasePortfolioConfig, create_config(config_type=ConfigType.PORTFOLIO)
+            )
+        repo_type = PortfolioRepositoryType.from_str(repo_type_str)
+        if repo_type not in PORTFOLIO_REPO_REGISTRY:
+            raise ValueError(f"Unsupported portfolio repository type: {repo_type}")
+        repo_class = PORTFOLIO_REPO_REGISTRY[repo_type]
+        repo_kwargs = PORTFOLIO_REPO_REGISTRY[repo_type](config)
+        return repo_class(**repo_kwargs)
